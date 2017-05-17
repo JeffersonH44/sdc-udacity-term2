99e327b3fd81103ec98769e613e87b795789f6ce
Jefferson Hernández <jeffersonh44@gmail.com>
2017-05-17 16:48:20 -0500

    jacobian and RMSE completed

--- project-1/src/tools.cpp
+++ project-1/src/tools.cpp
@@ -15,6 +15,39 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+
+    // TODO: YOUR CODE HERE
+
+    // check the validity of the following inputs:
+    //  * the estimation vector size should not be zero
+    //  * the estimation vector size should equal ground truth vector size
+    // ... your code here
+    if(estimations.size() == 0 || (estimations.size() != ground_truth.size())) {
+        std::cout << "estimations size invalid" << std::endl;
+        return rmse;
+    }
+
+    //accumulate squared residuals
+    for(int i=0; i < estimations.size(); ++i){
+        // ... your code here
+        VectorXd res = estimations[i] - ground_truth[i];
+        res = res.array() * res.array();
+        rmse += res;
+    }
+
+    //calculate the mean
+    // ... your code here
+    rmse = rmse.array() / estimations.size();
+
+    //calculate the squared root
+    // ... your code here
+    rmse = rmse.array().sqrt();
+
+    //return the result
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +55,36 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+    MatrixXd Hj(3,4);
+    //recover state parameters
+    double px = x_state(0);
+    double py = x_state(1);
+    double vx = x_state(2);
+    double vy = x_state(3);
+
+    //TODO: YOUR CODE HERE
+
+    //check division by zero
+    if(px == 0 && py == 0) {
+        std::cout << "Division by zero" << std::endl;
+    }
+
+    double px_2 = px * px;
+    double py_2 = py * py;
+
+    //compute the Jacobian matrix
+    double one = px / sqrt(px_2 + py_2);
+    double two = py / sqrt(px_2 + py_2);
+    double five = -(py / (px_2 + py_2));
+    double six = px / (px_2 + py_2);
+    double nine = (py*(vx*py - vy*px)) / pow(px_2 + py_2, 1.5);
+    double ten = (px*(vy*px - vx*py)) / pow(px_2 + py_2, 1.5);
+    double eleven = one;
+    double twelve = two;
+
+    Hj << one, two, 0, 0,
+            five, six, 0, 0,
+            nine, ten, eleven, twelve;
+
+    return Hj;
 }
